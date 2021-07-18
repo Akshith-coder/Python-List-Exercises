fc38619a75ee60fe69fe09a912396f96a5656942
AkshithKachaka1 <67799241+Akshith-coder@users.noreply.github.com>
2021-07-18 02:59:55 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,32 @@
+a = 45
+b = 46
+
+def max(a,b):
+    if a >= b:
+        return a
+    else:
+        return b
+
+
+print(max(a, b))
+
+
+maximum = max(a, b)
+print(maximum)
+
+
+def min(a, b):
+      
+    if a <= b:
+        return a
+    else:
+        return b
+print(min(a,b))
+
+minimum = min(a, b)
+print(minimum)
+
+
+x = [1, 2, 3, 4, 5]
+print("The length of list is: ", len(x))
+
